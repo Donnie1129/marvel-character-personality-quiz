1f9fbc3fbd6bc8e42472abcb2cbc15ab03d5983c
Suzet Salinas <suzet.sal@gmail.com>
2023-10-26 19:54:29 -0500

    created skeleton for questions array with placeholder questions/answers

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,29 @@
 // this is Javascript
+
+const questions = [
+    {
+        question: 'Where would you go on a dream vacation? ',
+        answers: ['Undisclosed location', 'New York City', 'Hawaii', 'Paris', 'I am Groot'],
+        
+      },
+      {
+        question: 'placeholder question #2',
+        answers: ['A', 'B', 'C', 'D', 'E'],
+        
+      },
+      {
+        question: 'placeholder question #3',
+        answers: ['A', 'B', 'C', 'D', 'E'],
+        
+      },
+      {
+        question: 'placeholder question #4',
+        answers: ['A', 'B', 'C', 'D', 'E'],
+        
+      },
+      {
+        question: 'placeholder question #5',
+        answers: ['A', 'B', 'C', 'D', 'E'],
+        
+      }
+];
