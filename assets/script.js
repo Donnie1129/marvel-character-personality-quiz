ded67acc686a57ad4a1b27a9216a3106edf7ef26
tyleryannes94 <tyler.yannes@gmail.com>
2023-10-26 20:20:46 -0500

    Updated the fetch query to include a list of 5 characters and running the fetch for each of the 5 characters.

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,31 @@
+const characters = ["Deadpool", "Captain America", "Groot", "Spider-Man (Peter Parker)", "Black Widow"];
 let ts = new Date().getTime();
 const privateKey = "c25596d5c722b3ccb5c94d495df884a0e237d83e";
 const publicKey = "80daa3778422ffa68ae1546ef689f700";
 let hash = CryptoJS.MD5(ts + privateKey + publicKey).toString();
-
 const baseUrl = "https://gateway.marvel.com/v1/public/characters";
 
-fetch(`${baseUrl}?ts=${ts}&apikey=${publicKey}&hash=${hash}`)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
+let characterData = [];
+
+characters.forEach(character => {
+    fetch(`${baseUrl}?name=${character}&ts=${ts}&apikey=${publicKey}&hash=${hash}`)
+        .then(response => response.json())
+        .then(data => {
+                console.log(data.data.results)
+        })
+        .catch(error => {
+            console.log('Error:', error);
+        });
+});
+
+// function getAllCharacters () {
+//     fetch(`${baseUrl}?&ts=${ts}&apikey=${publicKey}&hash=${hash}&nameStartsWith=Sp&limit=100`)
+//         .then(response => response.json())
+//         .then(data => {
+//                 console.log(data.data.results)
+//         })
+//         .catch(error => {
+//             console.log('Error:', error);
+//         });
+// };
+// getAllCharacters();
